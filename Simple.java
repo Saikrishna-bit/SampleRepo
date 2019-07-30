b086438a7277e14596ab21c4a122d8288629a07e
Sk <balaganisk9@gmail.com>
2019-07-30 12:00:40 +0530

    Initial commit1

--- Simple.java
+++ Simple.java
@@ -1,5 +1,22 @@
+class Calc{
+public float add(int x,int y){
+     return x+y;
+ }
+
+public float subtract(int x,int y){
+     return x-y;
+ }
+ public float multiply(int x,int y){
+     return x*y;
+ }
+ public float divide(int x,int y){
+     return x/y;
+ }
+}
 class Simple{  
+ 
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
 }  
+
